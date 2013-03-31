419e228f183604bbff94a717343499900f218feb
Alex Bereznevatiy <alex.bereznevatiy@gmail.com>
2013-03-31 22:46:15 +0300

    Safari bridge implementation

--- com.facedev.js.debug.safari.mac/include/fd_safari.h
+++ com.facedev.js.debug.safari.mac/include/fd_safari.h
@@ -1,5 +1,6 @@
 /*
  * Safari-specific functions.
+ * Bridge between safari browser nad java.
  *
  *  Created on: Mar 27, 2013
  *      Author: alex.bereznevatiy@gmail.com
@@ -8,7 +9,80 @@
 #ifndef FD_SAFARI_H_
 #define FD_SAFARI_H_
 
+#include "../../com.facedev.native.common/include/fd_common.h"
+#include <vector>
+#include <string>
 
+#define FD_SAFARI_INSTALLED ((fd_uint)1)
 
+#define FD_SAFARI_UNINSTALLED ((fd_uint)2)
+
+#define FD_SAFARI_NOT_CHANGED ((fd_uint)3)
+
+#define FD_SAFARI_ERROR ((fd_uint)4)
+
+namespace fd {
+
+class safari_bridge;
+
+class safari_instance {
+private:
+	friend class safari_bridge;
+
+	std::string _name;
+
+	safari_instance(std::string name) {
+		_name = name;
+	}
+
+	~safari_instance() {}
+public:
+	inline std::string name() {
+		return _name;
+	}
+};
+
+class safari_bridge {
+private:
+	static safari_bridge* instance;
+
+	safari_bridge();
+	~safari_bridge();
+public:
+	inline static safari_bridge* get() {
+		if (instance == fd_null) {
+			instance = new safari_bridge();
+		}
+		return instance;
+	}
+
+	/*
+	 * Installs this driver.
+	 * Returns FD_SAFARI_INSTALLED in case when installed,
+	 * FD_SAFARI_NOT_CHANGED in case when already installed and
+	 * FD_SAFARI_ERROR in case of error
+	 */
+	fd_uint install();
+
+	/*
+	 * Uninstalls this driver.
+	 * Returns FD_SAFARI_UNINSTALLED in case when uninstalled,
+	 * FD_SAFARI_NOT_CHANGED in case when already uninstalled and
+	 * FD_SAFARI_ERROR in case of error
+	 */
+	fd_uint uninstall();
+
+	/*
+	 * Returns list of instances of running safari windows.
+	 */
+	std::vector<safari_instance*> list();
+
+	/*
+	 * Returns name of this driver.
+	 */
+	const char* name();
+};
+
+} // namespace fd
 
 #endif /* FD_SAFARI_H_ */
--- com.facedev.js.debug.safari/src/com/facedev/js/debug/safari/SafariJsDebugger.java
+++ com.facedev.js.debug.safari/src/com/facedev/js/debug/safari/SafariJsDebugger.java
@@ -1,7 +1,12 @@
 package com.facedev.js.debug.safari;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
 
 import com.facedev.js.debug.JsDebugger;
 import com.facedev.js.debug.JsDebuggerException;
@@ -9,37 +14,177 @@ import com.facedev.js.debug.JsDebuggerInstance;
 import com.facedev.js.debug.JsDebuggerInstanceListener;
 
 public class SafariJsDebugger implements JsDebugger {
+	
+	private static final Object LOCK = new Object();
+	private static final String LIBRARY_NAME = "safari_debug_mac";
+	
+	private static final List<JsDebuggerInstanceListener> listeners = 
+			new CopyOnWriteArrayList<JsDebuggerInstanceListener>();
+	
+	private static volatile SafariJsDebugger singletonInstance;
+	
+	private static volatile FutureTask<List<JsDebuggerInstance>> instances;
+	
+	private static volatile String name = "Safari";
+	
+	private static volatile Boolean supported;
+	
+	private static volatile DispatcherThread dispatcher;
 
 	public SafariJsDebugger() {
-		// TODO Auto-generated constructor stub
+		synchronized(LOCK) {
+			if (singletonInstance != null) {
+				throw new IllegalStateException();
+			}
+			singletonInstance = this;
+			
+			registerNatives();
+		}
+	}
+	
+	private static void registerNatives() {
+		if (supported != null) {
+			// already registered
+			return;
+		}
+		try {
+			System.loadLibrary(LIBRARY_NAME);
+			boolean supported = initSafariDriver();
+			name = getDriverName();
+			refreshInstances();
+			dispatcher = new DispatcherThread();
+			dispatcher.start();
+			SafariJsDebugger.supported = supported;
+		} catch (UnsatisfiedLinkError ex) {
+			supported = Boolean.FALSE;
+		}
+		
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see com.facedev.js.debug.JsDebugger#getRegisteredInstances()
+	 */
 	public List<JsDebuggerInstance> getRegisteredInstances()
 			throws JsDebuggerException {
-		return Collections.emptyList();
+		if (!isSupported() || instances == null) {
+			throw new JsDebuggerException("Safari Debugger is not loaded or not supported on this platform");
+		}
+		try {
+			return instances.get();
+		} catch (ExecutionException ex) {
+			throw new JsDebuggerException("Unable to get instances", ex);
+		} catch (InterruptedException ex) {
+			throw new JsDebuggerException("Unable to get instances", ex);
+		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see com.facedev.js.debug.JsDebugger#getName()
+	 */
 	public String getName() {
-		return "Safari";
+		return name;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see com.facedev.js.debug.JsDebugger#isSupported()
+	 */
 	public boolean isSupported() {
-		return true;
+		return supported != null && supported;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see com.facedev.js.debug.JsDebugger#dispose()
+	 */
 	public void dispose() {
-		// TODO Auto-generated method stub
-
+		synchronized(LOCK) {
+			listeners.clear();
+			supported = null;
+			singletonInstance = null;
+		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see com.facedev.js.debug.JsDebugger#addInstanceListener(com.facedev.js.debug.JsDebuggerInstanceListener)
+	 */
 	public void addInstanceListener(JsDebuggerInstanceListener listener) {
-		// TODO Auto-generated method stub
-
+		listeners.add(listener);
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see com.facedev.js.debug.JsDebugger#removeInstanceListener(com.facedev.js.debug.JsDebuggerInstanceListener)
+	 */
 	public void removeInstanceListener(JsDebuggerInstanceListener listener) {
-		// TODO Auto-generated method stub
+		listeners.remove(listener);
+	}
+	
+	private static void refreshInstances() {
+		instances = new FutureTask<List<JsDebuggerInstance>>(
+				new Callable<List<JsDebuggerInstance>>() {
 
+			public List<JsDebuggerInstance> call() throws Exception {
+				List<JsDebuggerInstance> result = new ArrayList<JsDebuggerInstance>();
+				
+				int index = 0;
+				SafariJsDebuggerInstance instance;
+				while (fillSafariInstance(instance = new SafariJsDebuggerInstance(), index++)) {
+					result.add(instance);
+				}
+					
+				return Collections.unmodifiableList(result);
+			}
+			
+		});
 	}
 
+	private static native boolean initSafariDriver();
+	
+	private static native void disposeSafariDriver();
+	
+	private static native boolean fillSafariInstance(SafariJsDebuggerInstance instance, int index);
+	
+	private static native String getDriverName();
+	
+	private static class DispatcherThread extends Thread {
+		
+		DispatcherThread() {
+			setDaemon(true);
+		}
+
+		@Override
+		public void run() {
+			while (this == dispatcher) {
+				try {
+					refresh();
+				} catch (InterruptedException ex) {
+					// ignore & return
+					return;
+				} catch (ExecutionException ex) {
+					ex.printStackTrace();
+					return;
+				}
+			}
+		}
+
+		private synchronized void refresh() throws InterruptedException, ExecutionException {
+			wait(500);
+			instances.run();
+			List<JsDebuggerInstance> previous = instances.get();
+			refreshInstances();
+			instances.run();
+			List<JsDebuggerInstance> current = instances.get();
+			notifyListeners(previous, current);
+		}
+
+		private void notifyListeners(List<JsDebuggerInstance> previous,
+				List<JsDebuggerInstance> current) {
+			// TODO Auto-generated method stub
+			
+		}
+	}
 }
